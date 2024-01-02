b1507504817cd9d2d92564a2529717172d2efec6
thbe <code@thbe.org>
2024-01-02 23:34:57 +0100

    Fix cue converter

--- generic/convert_cue_file_into_yt_tracklist.py
+++ generic/convert_cue_file_into_yt_tracklist.py
@@ -15,6 +15,8 @@ Purpose:      Script to convert cue files into YT track lists
 
 import re
 import sys
+import datetime
+
 
 if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} <directory_path>")
@@ -23,16 +25,21 @@ if len(sys.argv) != 2:
 with open(sys.argv[1], "r", encoding="utf-8") as f:
     file_content = f.read()
 
-artist_pattern = re.compile(r"\t\tPERFORMER \"\w+\.?\w*\"")
-title_pattern = re.compile(r"\t\tTITLE \"\w+\.?\w*\"")
-index_pattern = re.compile(r"\t\tINDEX \d+ (\d+) (.*)")
+artist_pattern = re.compile(r'[\t\t| +]PERFORMER "(.*)"')
+title_pattern = re.compile(r'[\t\t| +]TITLE "(.*)"')
+index_pattern = re.compile(r'[\t\t| +]INDEX \d\d (.*)')
 
 artists = artist_pattern.findall(file_content)
 titles = title_pattern.findall(file_content)
 indices = index_pattern.findall(file_content)
 
-tracklist = []
-for i, (title, artist, time) in enumerate(zip(titles, artists, indices)):
-    tracklist += f"{time:02d} : {artist} - {title}\n"
-
-print(tracklist)
+for i, dataset in enumerate(zip(indices, artists, titles)):
+    split_minutes, split_seconds, split_frames = dataset[0].split(':')
+    total_seconds = int(split_minutes) * 60 + int(split_seconds) + int(split_frames) / 75
+    timedelta_object = datetime.timedelta(seconds=total_seconds)
+    converted_hours = timedelta_object.seconds // 3600
+    converted_minutes = (timedelta_object.seconds % 3600) // 60
+    converted_seconds = timedelta_object.seconds % 60
+    converted_time = "{:02d}:{:02d}:{:02d}".format(converted_hours, converted_minutes, converted_seconds)
+    trackinfo = converted_time + ' : ' + dataset[1] + ' - ' + dataset[2]
+    print(trackinfo)
